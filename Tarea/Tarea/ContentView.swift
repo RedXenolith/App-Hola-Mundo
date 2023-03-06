93c1a88ba92f60b442766da5cd5e7615f7b99cf5
Go.Diego_Code <54591302+RedXenolith@users.noreply.github.com>
2023-03-06 14:29:06 -0500

    Add files via upload

---
+++ Tarea/Tarea/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Tarea
+//
+//  Created by Diego Guzmán on 6/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola mundo")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
